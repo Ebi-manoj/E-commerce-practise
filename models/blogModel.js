1316abb2b1e75a92bd5f093fa20f9d98f88062a3
Ebi manoj <ebimanoj28@gmail.com>
2024-12-04 18:38:57 +0530

    Blog

---
+++ models/blogModel.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    image: {
+      type: String,
+    },
+    author: {
+      type: String,
+      default: 'admin',
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+//Export the model
+export const Blog = mongoose.model('Blog', BlogSchema);
